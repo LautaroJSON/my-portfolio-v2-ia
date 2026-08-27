e9c18d5f3dd852d8d037ae5c9b722a8fb28a87bc
Lautaro Fernandez <fernandez.n.lautaro@gmail.com>
2026-08-27 07:27:53 +0000

    fix: calculare line count

--- src/lib/line-count.ts
+++ src/lib/line-count.ts
@@ -1,10 +1,7 @@
 const LINE_HEIGHT_PX = 20;
 
 export const calculateLineCount = (contentElement: HTMLElement): number => {
-  const styles = getComputedStyle(contentElement);
-  const paddingTop = parseFloat(styles.paddingTop);
-  const paddingBottom = parseFloat(styles.paddingBottom);
-  const rawHeight = contentElement.scrollHeight - paddingTop - paddingBottom;
+  const rawHeight = contentElement.scrollHeight;
   const contentHeight = Math.round(rawHeight);
 
   return Math.floor(contentHeight / LINE_HEIGHT_PX);
